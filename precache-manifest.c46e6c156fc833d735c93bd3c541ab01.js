0ddc215feb754276d64685a6cfb0e63cd029bafc
Gustavo Cotta <gmcotta34@gmail.com>
2020-03-21 20:49:00 -0300

    Updates

---
+++ precache-manifest.c46e6c156fc833d735c93bd3c541ab01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "369613888664063e3b96e8b83011618b",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "ace910232fe172be2310",
+    "url": "/monster-rolodex/static/css/main.3eb7471e.chunk.css"
+  },
+  {
+    "revision": "f05237ce14ba31f66c50",
+    "url": "/monster-rolodex/static/js/2.66d1f31a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.66d1f31a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ace910232fe172be2310",
+    "url": "/monster-rolodex/static/js/main.c03928a6.chunk.js"
+  },
+  {
+    "revision": "bc09d86fba44e1c7ba83",
+    "url": "/monster-rolodex/static/js/runtime-main.8000f3b5.js"
+  }
+]);
